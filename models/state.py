2313c6ac81d94b73982c5ae46b101d9d70c1faad
oneharry <harryofficialchidi@yahoo.com>
2023-04-23 14:50:07 +0000

    fixed task 7

--- models/state.py
+++ models/state.py
@@ -11,11 +11,10 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade="all, delete, delete-orphan")
-    else:
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="delete")
+    if getenv("HBNB_TYPE_STORAGE") != "db":
 
         @property
         def cities(self):
---
+++ web_flask/9-states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    Starts a flask web application
+"""
+
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(err):
+    """ Remove the current sqlalchemy session"""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """ Render list of all the states """
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_id(id):
+    """ Render a state based on the id """
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
